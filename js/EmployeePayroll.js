226dd4e3c19b0e1968a5ebfd6548311b8d3b9faa
mehulbhange <bhangemehul@gmail.com>
2022-09-30 17:58:50 +0530

    ADDED:[Mehul] added resetForm method to reset form fields

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -141,4 +141,27 @@ function getFromLocalStorage(){
     }
 }
 
+const resetForm = () =>{
+    console.log("reset called ")
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','0');
+    setValue('#year','2022');
+}
 
+const unsetSelectedValues = (propertyValue) =>{
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach( item => { 
+        item.checked = false;
+    }); 
+}
+
+const setValue = (id, value) =>{
+    const element = document.querySelector(id);
+    element.value = value;
+}
